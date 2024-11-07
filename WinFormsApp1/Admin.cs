e0bc2270533251c3e8f251608965b1faf80ee6a5
Konstantin Mitkov <kocevelikibg06@gmail.com>
2024-11-07 20:01:24 +0200

    Startin Admin form

--- WinFormsApp1/Admin.cs
+++ WinFormsApp1/Admin.cs
@@ -14,7 +14,18 @@ namespace WinFormsApp1
     {
         public Admin()
         {
+            richTextBox1.ReadOnly = true;
             InitializeComponent();
+        }
+        ControllerDrivers controllerDrivers;
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = controllerDrivers.Drivers();
+        }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            controllerDrivers = new ControllerDrivers();
         }
     }
 }
